ea8b6e5c326c292eb1b172cfcd2d4bb8eb297d88
ruchithasevakula <ruchithasony1999@gmail.com>
2021-09-07 14:47:55 +0530

    done

---
+++ 3_Implementation/inc/Quiz.h
@@ -0,0 +1,22 @@
+/**
+ * @file Quiz.h
+ * @author Ruchithasevakula (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef QUIZ_H
+#define QUIZ_H
+
+#include <stdio.h>
+#include "quiz_making.h"
+
+
+void show_record();
+void reset_score();
+void help();
+void edit_score(float , char []);
+#endif
