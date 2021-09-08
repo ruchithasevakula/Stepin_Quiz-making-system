94f520164e4af2829695cbc2ed3210c3e925d320
ruchithasevakula <ruchithasony1999@gmail.com>
2021-09-08 16:23:50 +0530

    updated

--- 3_Implementation/inc/header.h
+++ 3_Implementation/inc/header.h
@@ -8,15 +8,12 @@
  * @copyright Copyright (c) 2021
  * 
  */
-#ifndef QUIZ_H
-#define QUIZ_H
-
-#include <stdio.h>
-#include "quiz_making.h"
-
-
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
 void show_record();
 void reset_score();
 void help();
 void edit_score(float , char []);
-#endif
